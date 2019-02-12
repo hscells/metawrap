a7c531e563b873767600b8b109f68350f6d7487e
Harry Scells <harryscells@gmail.com>
2019-02-12 10:00:35 +1000

    Add http client.

---
+++ httpclient.go
@@ -0,0 +1,36 @@
+package metawrap
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"path"
+)
+
+type HTTPClient struct {
+	URL    string
+	client http.Client
+}
+
+func (c HTTPClient) Candidates(text string) (candidates []MappingCandidate, err error) {
+	req, err := http.NewRequest("POST", path.Join(c.URL, "/mm/candidates"), bytes.NewBufferString(text))
+	if err != nil {
+		return
+	}
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return
+	}
+	if resp.ContentLength == 0 {
+		return
+	}
+	err = json.NewDecoder(resp.Body).Decode(&candidates)
+	if err != nil {
+		return
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		return
+	}
+	return
+}
